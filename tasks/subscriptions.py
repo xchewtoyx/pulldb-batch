7f7ba2cec02c18c172b85822f254aa1c7264d20b
Russell Heilling <russell@linx.net>
2014-06-22 22:57:45 +0100

    Add subscription sharding

--- tasks/subscriptions.py
+++ tasks/subscriptions.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from functools import partial
 import json
 import logging
@@ -12,15 +13,48 @@ from pulldb.models import subscriptions
 
 # pylint: disable=W0232,C0103,E1101,R0201,R0903
 
+class ReshardSubs(TaskHandler):
+    @ndb.tasklet
+    def reshard_subs(self, subscription):
+        changed = False
+        if subscription.identifier:
+            sub_id = subscription.identifier
+        else:
+            sub_id = int(subscription.key.id())
+            subscription.identifier = sub_id
+            changed = True
+        if sub_id % 24 != subscription.shard:
+            subscription.shard = sub_id % 24
+            changed = True
+        if changed:
+            update = yield subscription.put_async()
+            raise ndb.Return(update)
+
+    def get(self):
+        query = subscriptions.Subscription.query()
+        updates = query.map(self.reshard_subs)
+        update_count = sum(1 for update in updates if update)
+        message = 'Resharded %d of %d subscriptions' % (
+            update_count, len(updates)
+        )
+        logging.info(message)
+        self.response.write(json.dumps({
+            'status': 200,
+            'message': message,
+        }))
+
 class UpdateSubs(TaskHandler):
     @ndb.tasklet
     def locate_pulls(self, subscription, issue):
-        pull_key = pulls.pull_key(
-            issue.key.id(), user=subscription.key.parent(), create=False
-        )
+        issue_key = issue.key
+        user_key = subscription.key.parent()
+        pull_key = pulls.pull_key(issue_key, user=user_key, create=False)
         pull = yield pull_key.get_async()
         if not pull:
-            raise ndb.Return(pull_key)
+            pull = pulls.pull_key(
+                    issue_key, user=user_key, create=True, batch=True
+            )
+            raise ndb.Return(pull)
 
     @ndb.tasklet
     def check_pulls(self, subscription):
@@ -30,16 +64,22 @@ class UpdateSubs(TaskHandler):
         )
         pull_callback = partial(self.locate_pulls, subscription)
         new_pulls = issue_query.map(pull_callback)
-        raise ndb.Return([pull for pull in new_pulls if pull])
+        new_pulls = [pull for pull in new_pulls if pull]
+        if new_pulls:
+            logging.info('Adding %d new pulls for sub %r',
+                         len(new_pulls), subscription.key)
+            yield ndb.put_multi_async(new_pulls)
+        raise ndb.Return(new_pulls)
 
     def get(self):
-        query = subscriptions.Subscription.query()
-        new_pulls = query.map(self.check_pulls)
-        pull_count = sum(len(pull) for pull in new_pulls if pull)
+        current_shard = datetime.now().hour
+        query = subscriptions.Subscription.query(
+            subscriptions.Subscription.shard == current_shard,
+        )
+        updates = query.map(self.check_pulls)
         self.response.write(json.dumps({
             'status': 200,
-            'seen': len(new_pulls),
-            'updated': pull_count,
+            'updated': len(updates),
         }))
 
 class Validate(TaskHandler):
@@ -62,6 +102,7 @@ class Validate(TaskHandler):
         }))
 
 app = create_app([
+    Route('/tasks/subscriptions/reshard', ReshardSubs),
     Route('/tasks/subscriptions/update', UpdateSubs),
     Route('/tasks/subscriptions/validate', Validate),
 ])
