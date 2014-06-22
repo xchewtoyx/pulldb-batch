c2cd9c23c17228d38f56826849524ddac904f7e2
Russell Heilling <russell@linx.net>
2014-06-22 02:17:51 +0100

    Update to latest common and add task handler to add pulls for new issues

--- tasks/subscriptions.py
+++ tasks/subscriptions.py
@@ -1,3 +1,4 @@
+from functools import partial
 import json
 import logging
 
@@ -5,9 +6,42 @@ from google.appengine.ext import ndb
 
 # pylint: disable=F0401
 from pulldb.base import create_app, Route, TaskHandler
+from pulldb.models import issues
+from pulldb.models import pulls
 from pulldb.models import subscriptions
 
 # pylint: disable=W0232,C0103,E1101,R0201,R0903
+
+class UpdateSubs(TaskHandler):
+    @ndb.tasklet
+    def locate_pulls(self, subscription, issue):
+        # this has already been cached, so no need to go async
+        query = pulls.Pull.query(
+            pulls.Pull.issue == issue.key,
+            pulls.Pull.subscription == subscription.key,
+        )
+        pull = yield query.get_async()
+        if not pull:
+            raise ndb.Return(pulls.pull_key(issue))
+
+    @ndb.tasklet
+    def check_pulls(self, subscription):
+        issue_query = issues.Issue.query(
+            issues.Issue.volume == subscription.volume
+        )
+        pull_callback = partial(self.locate_pulls, subscription)
+        new_pulls = issue_query.map(pull_callback)
+        raise ndb.Return([pull for pull in new_pulls if pull])
+
+    def get(self):
+        query = subscriptions.Subscription.query()
+        new_pulls = query.map(self.check_pulls)
+        pull_count = sum(len(pull) for pull in new_pulls if pull)
+        self.response.write(json.dumps({
+            'status': 200,
+            'seen': len(new_pulls),
+            'updated': pull_count,
+        }))
 
 class Validate(TaskHandler):
     @ndb.tasklet
@@ -29,5 +63,6 @@ class Validate(TaskHandler):
         }))
 
 app = create_app([
+    Route('/tasks/subscriptions/update', UpdateSubs),
     Route('/tasks/subscriptions/validate', Validate),
 ])
