939e427729155c81ea864ce7eda13b2510a0b79d
Russell Heilling <russell@linx.net>
2014-06-22 21:08:28 +0100

    Add some extra attributes to fix in the pulls refresh controller

--- tasks/pulls.py
+++ tasks/pulls.py
@@ -5,8 +5,10 @@ from google.appengine.ext import ndb
 
 # pylint: disable=F0401
 from pulldb.base import create_app, Route, TaskHandler
+from pulldb.models.base import model_to_dict
 from pulldb.models import pulls
 from pulldb.models import subscriptions
+from pulldb.models import volumes
 
 # pylint: disable=W0232,C0103,E1101,R0201,R0903
 
@@ -60,6 +62,50 @@ class StreamSelect(TaskHandler):
             'message': message,
         }))
 
+class Refresh(TaskHandler):
+    @ndb.tasklet
+    def refresh_pull(self, pull):
+        changed = False
+        if not pull.read:
+            logging.info('Setting missing read attribute to False %r', pull.key)
+            pull.read = False
+            changed = True
+        if pull.issue and not pull.volume:
+            logging.info('Adding missing volume to pull %r', pull.key)
+            issue = yield pull.issue.get_async()
+            pull.volume = issue.volume
+            changed = True
+        if pull.volume and not pull.subscription:
+            logging.info('Adding missing subscription to pull %r', pull.key)
+            query = subscriptions.Subscription.query(
+                subscriptions.Subscription.volume == pull.volume,
+                ancestor=pull.key.parent()
+            )
+            subscription = yield query.get_async()
+            if subscription:
+                pull.subscription = subscription.key
+                changed = True
+        if pull.read and not pull.pulled:
+            logging.info('Marking read pull %r as pulled', pull.key)
+            pull.pulled = True
+            changed = True
+        if changed:
+            yield pull.put_async()
+            raise ndb.Return({
+                'pull': model_to_dict(pull)
+            })
+
+    def get(self):
+        query = pulls.Pull.query()
+        results = query.map(self.refresh_pull)
+        update_count = sum(1 for pull in results if pull)
+        message = '%d of %d pulls refreshed' % (update_count, len(results))
+        self.response.write({
+            'status': 200,
+            'message': ' refreshed',
+            'results': [pull for pull in results if pull],
+        })
+
 class Validate(TaskHandler):
     @ndb.tasklet
     def drop_invalid(self, pull):
@@ -81,5 +127,6 @@ class Validate(TaskHandler):
 
 app = create_app([
     Route('/tasks/pulls/update/streams', StreamSelect),
+    Route('/tasks/pulls/refresh', Refresh),
     Route('/tasks/pulls/validate', Validate),
 ])
