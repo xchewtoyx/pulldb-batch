c266529a4c164747104c189dde72623d81e5dd17
Russell Heilling <russell@linx.net>
2015-04-25 22:44:46 +0100

    Calculate / update the start attribute of streams when updating counts

--- tasks/streams.py
+++ tasks/streams.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import logging
 
@@ -8,34 +9,58 @@ from pulldb.base import create_app, Route, TaskHandler
 from pulldb.models import pulls
 from pulldb.models import streams
 from pulldb.models import subscriptions
+from pulldb.varz import VarzContext
 
 # pylint: disable=W0232,C0103,E1101,R0201,R0903
 
 class StreamTotals(TaskHandler):
     @ndb.tasklet
     def count_unread(self, stream):
+        stream_changed = False
+
         query = pulls.Pull.query(
             pulls.Pull.stream == stream.key,
             pulls.Pull.pulled == True,
             pulls.Pull.read == False,
             ancestor=stream.key.parent(),
-        )
-        matches = yield query.fetch_async(keys_only=True)
-        if not stream.length or stream.length != len(matches):
-            stream.length = len(matches)
+        ).order(pulls.Pull.pubdate)
+
+        count_pulls = query.count_async()
+        fetch_first = query.fetch_async(limit=1)
+        pull_count, first_pull = yield count_pulls, fetch_first
+
+        if pull_count:
+            first_pull = first_pull[0]
+
+        if pull_count and first_pull.pubdate:
+            if first_pull.pubdate != stream.start:
+                stream.start = first_pull.pubdate
+                stream_changed = True
+
+        if stream.length != pull_count:
+            stream.length = pull_count
+            stream_changed = True
+
+        if stream_changed:
             yield stream.put_async()
             raise ndb.Return(True)
 
+    @VarzContext('streams')
     def get(self):
+        varz = self.varz
+        varz.action = 'updatecounts'
         query = streams.Stream.query()
         results = query.map(self.count_unread)
         changed = sum(1 for result in results if result)
+        streams_seen = len(results)
+        varz.streams = streams_seen
+        varz.changed = changed
         logging.info('Saw %d streams, with %d changes' % (
-            len(results), changed,
+            streams_seen, changed
         ))
         self.response.write(json.dumps({
             'status': 200,
-            'seen': len(results),
+            'seen': streams_seen,
             'changed': changed,
         }))
 
@@ -61,17 +86,25 @@ class StreamWeights(TaskHandler):
         yield ndb.put_multi_async(changed)
         raise ndb.Return(len(changed))
 
+    @VarzContext('streams')
     def get(self):
+        varz = self.varz
+        varz.action = 'updatecounts'
         query = streams.Stream.query()
         update_list = query.map(self.update_weight)
         updated = sum(count for count in update_list if count)
+        streams_seen = len(update_list)
+        varz.updated = updated
+        varz.streams = streams_seen
         message = 'Updated weights for %d pulls in %d streams' % (
-            updated, len(update_list),
+            updated, streams_seen,
         )
         logging.info(message)
         self.response.write(json.dumps({
             'status': 200,
             'message': message,
+            'streams': streams_seen,
+            'updated': updated,
         }))
 
 app = create_app([
