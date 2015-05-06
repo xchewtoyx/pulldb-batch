bc2edb51a07a6f1e15abca14a42e9f197e8019ab
Russell Heilling <russell@linx.net>
2015-05-06 15:39:55 +0100

    Update issue refresh to queue based rather than purely shard based

--- tasks/issues.py
+++ tasks/issues.py
@@ -4,6 +4,7 @@ from functools import partial
 import json
 import logging
 import sys
+import time
 from zlib import crc32
 
 from google.appengine.api import search
@@ -15,6 +16,7 @@ from pulldb.models import arcs
 from pulldb.models import comicvine
 from pulldb.models import issues
 from pulldb.models import volumes
+from pulldb.varz import VarzContext
 
 class FetchNew(TaskHandler):
     @ndb.tasklet
@@ -33,6 +35,20 @@ class FetchNew(TaskHandler):
         collections = yield ndb.get_multi_async(arc_keys)
         raise ndb.Return(issue, any(collections))
 
+    def check_batch(self, issue_batch):
+        issue_futures = [self.check_issue(issue) for issue in issue_batch]
+        added_issues = []
+        skipped_issues = []
+        for future in issue_futures:
+            issue_dict, candidate = future.get_result()
+            if candidate:
+                issue = issues.issue_key(
+                    issue_dict, create=True, batch=True)
+                added_issues.append(issue)
+            else:
+                skipped_issues.append(issue_dict['id'])
+        return added_issues, skipped_issues
+
     @ndb.toplevel
     def get(self):
         self.cv_api = comicvine.load()
@@ -48,17 +64,14 @@ class FetchNew(TaskHandler):
         new_issues = [
             issue for issue in new_issues if isinstance(issue, dict)
         ]
-        issue_futures = [self.check_issue(issue) for issue in new_issues]
         added_issues = []
         skipped_issues = []
-        for future in issue_futures:
-            issue_dict, candidate = future.get_result()
-            if candidate:
-                issue = issues.issue_key(
-                    issue_dict, create=True, batch=True)
-                added_issues.append(issue)
-            else:
-                skipped_issues.append(issue_dict['id'])
+        # Fetch 30 at a time to avoid too many updates in parallel
+        for i in range(0, len(new_issues), 30):
+            issue_slice = new_issues[i:i+30]
+            batch_added, batch_skipped = self.check_batch(issue_slice)
+            added_issues += batch_added
+            skipped_issues += batch_skipped
 
         status = 'New issues: %d found, %d added, %d skipped' % (
             len(new_issues),
@@ -70,6 +83,87 @@ class FetchNew(TaskHandler):
             'status': 200,
             'message': status,
         }))
+
+
+class RequeueShard(TaskHandler):
+    @ndb.tasklet
+    def requeue_issue(self, issue):
+        if issue.complete:
+            issue.complete = False
+            yield issue.put_async()
+            raise ndb.Return(True)
+
+    def shard_filter(self):
+        shard_key = Setting.query(
+            Setting.name == 'issue_shard_count').get()
+        shard = self.request.get('shard')
+        if not shard:
+            shard = int(time.time() // 3600) % int(shard_key.value)
+        return shard
+
+    def get(self):
+        logging.info('Recursion limit: %d', sys.getrecursionlimit())
+        shard = self.shard_filter()
+        query = issues.Issue.query(
+            issues.Issue.shard == int(shard),
+        )
+        issue_count = query.count_async()
+        updates = query.map(self.requeue_issue)
+        update_count = sum(1 for updated in updates if updated)
+        issue_count = issue_count.get_result()
+        status = 'Updated %d of %d issues' % (
+            update_count, issue_count)
+
+        logging.info(status)
+        self.response.write(json.dumps({
+            'status': 200,
+            'message': status,
+            'shard': shard,
+            'issues': issue_count,
+            'updates': update_count,
+        }))
+
+class RefreshBatch(TaskHandler):
+    @ndb.tasklet
+    def refresh_issue(self, issue):
+        issue_dict = yield self.cv_api.fetch_issue_async(
+            int(issue.key.id()))
+        if not issue_dict:
+            issue_updated = False
+            logging.warn('Cannot update issue: %r', issue_dict)
+            raise ndb.Return(None)
+        issue_updated, last_update = issue.has_updates(issue_dict)
+        if issue_updated:
+            logging.debug('Issue %r updated', issue.key)
+            issue.apply_changes(issue_dict)
+        issue.complete = True
+        yield issue.put_async()
+        raise ndb.Return(issue_updated)
+
+    @VarzContext('issue_queue')
+    def get(self):
+        # pylint: disable=unused-variable, attribute-defined-outside-init
+        logging.info('Recursion limit: %d', sys.getrecursionlimit())
+        self.cv_api = comicvine.load()
+        query = issues.Issue.query(
+            issues.Issue.complete == False,
+        )
+        issue_count = query.count_async()
+        self.varz.update_count = 0
+        for offset in range(0, 100, 50):
+            updates = query.map(self.refresh_issue, offset=offset, limit=50)
+            self.varz.update_count += sum(1 for updated in updates if updated)
+        self.varz.backlog = issue_count = issue_count.get_result()
+        status = 'Updated %d of %d issues' % (
+            self.varz.update_count, self.varz.backlog)
+        logging.info(status)
+        self.response.write(json.dumps({
+            'status': 200,
+            'message': status,
+            'issues': self.varz.backlog,
+            'updates': self.varz.update_count,
+        }))
+
 
 class RefreshShard(TaskHandler):
     @ndb.tasklet
@@ -88,16 +182,21 @@ class RefreshShard(TaskHandler):
         logging.debug('Issue %r updated', issue.key)
         raise ndb.Return(issue_updated)
 
+    def shard_filter(self):
+        shard_key = Setting.query(
+            Setting.name == 'issue_shard_count').get()
+        shard = self.request.get('shard')
+        if not shard:
+            shard = int(time.time() // 3600) % int(shard_key.value)
+        return shard
+
     def get(self, *args): # pylint: disable=unused-argument
         # pylint: disable=unused-variable, attribute-defined-outside-init
         logging.info('Recursion limit: %d', sys.getrecursionlimit())
         self.cv_api = comicvine.load()
-        shard = self.request.get('shard')
-        if not shard:
-            shard = datetime.today().hour + 24 * date.today().weekday()
+        shard = self.shard_filter()
         query = issues.Issue.query(
             issues.Issue.shard == int(shard),
-            issues.Issue.volume > None, # issue has volume
         )
         issue_count = query.count()
         update_count = 0
@@ -141,26 +240,42 @@ class Reindex(TaskHandler):
             else:
                 ndb.put_multi(issue_list)
 
+
 class ReshardIssues(TaskHandler):
     @ndb.tasklet
-    def reshard_task(self, shards, issue):
-        seed = crc32(str(issue.identifier))
-        issue.shard = seed % shards
-        result = yield issue.put_async()
-        raise ndb.Return(result)
+    def reshard_task(self, shard_count, issue):
+        seed = crc32(issue.key.urlsafe())
+        shard = seed % shard_count
+        if issue.shard != shard:
+            issue.shard = shard
+            result = yield issue.put_async()
+            raise ndb.Return(result)
 
     def get(self):
-        shards_key = Setting.query(Setting.name == 'update_shards_key').get()
+        shards_key = Setting.query(Setting.name == 'issue_shard_count').get()
         shards = int(shards_key.value)
         callback = partial(self.reshard_task, shards)
         query = issues.Issue.query()
-        if not self.request.get('all'):
+        if self.request.get('shard'):
+            shard = int(self.request.get('shard'))
+            query = query.filter(issues.Issue.shard == shard)
+        if self.request.get('all'):
+            if self.request.get('shard_start'):
+                start = int(self.request.get('shard_start')) - 1
+                query = query.filter(issues.Issue.shard >= start)
+            if self.request.get('shard_stop'):
+                stop = int(self.request.get('shard_stop'))
+                query = query.filter(issues.Issue.shard < stop)
+        else:
             query = query.filter(issues.Issue.shard == -1)
+        issue_count = query.count_async()
         results = query.map(callback)
+        update_count = sum(1 for result in results if result)
         logging.info('Resharded %d issues', len(results))
         self.response.write(json.dumps({
             'status': 200,
-            'message': '%d issues resharded' % len(results),
+            'message': '%d of %d issues resharded' % (
+                update_count, issue_count.get_result()),
         }))
 
 class ValidateShard(TaskHandler):
@@ -231,6 +346,8 @@ class FixVolumeKey(TaskHandler):
 app = create_app([ # pylint: disable=invalid-name
     Route('/tasks/issues/convert', ConvertIssues),
     Route('/tasks/issues/fetchnew', FetchNew),
+    Route('/tasks/issues/requeue', RequeueShard),
+    Route('/tasks/issues/refresh/batch', RefreshBatch),
     Route('/<:batch|tasks>/issues/refresh', RefreshShard),
     Route('/tasks/issues/reindex', Reindex),
     Route('/tasks/issues/reshard', ReshardIssues),
