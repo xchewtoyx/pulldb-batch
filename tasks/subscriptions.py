cd0ac0bb20309b8be20002a404019139bd3d898c
Russell Heilling <russell@linx.net>
2015-05-13 11:30:34 +0100

    Add code for refreshing subscription details (i.e. mark fresh if there has been a new issue in the last 90 days)
    Add code for requeueing volumes associated with a subscription

--- tasks/subscriptions.py
+++ tasks/subscriptions.py
@@ -2,17 +2,119 @@ from datetime import datetime
 from functools import partial
 import json
 import logging
+import time
 from zlib import crc32
 
 from google.appengine.ext import ndb
 
 # pylint: disable=F0401
 from pulldb.base import create_app, Route, TaskHandler
+from pulldb.models import admin
 from pulldb.models import issues
 from pulldb.models import pulls
 from pulldb.models import subscriptions
 
 # pylint: disable=W0232,C0103,E1101,R0201,R0903
+
+class QueueActiveVolumes(TaskHandler):
+    @ndb.tasklet
+    def queue_collections(self, subscription):
+        collection = yield subscription.volume.get_async()
+        if collection.complete:
+            collection.complete = False
+            yield collection.put_async()
+            raise ndb.Return(collection)
+
+    def active_shard(self, offset=3):
+        if self.request.get('shard'):
+            return int(self.request.get('shard'))
+        else:
+            return current_shard(offset=offset)
+
+    def get(self):
+        shard = self.active_shard()
+        # TODO(rgh): Should restrict this to fresh once fresh is being set
+        #            by the subscription refresh task
+        query = subscriptions.Subscription.query(
+            subscriptions.Subscription.shard == shard,
+        )
+        count_future = query.count_async()
+        queued = query.map(self.queue_collections)
+        collection_count = count_future.get_result()
+        updated = sum(1 for collection in queued if collection)
+        message = 'Updated %d of %d collections in shard %d' % (
+            updated, collection_count, shard)
+        logging.info(message)
+        self.response.write({
+            'status': 200,
+            'message': message,
+            'total': collection_count,
+            'queued': updated,
+            'shard': shard,
+        })
+
+
+class RefreshSubscription(TaskHandler):
+    @ndb.tasklet
+    def check_freshness(self, subscription):
+        changed = False
+        issue_query = issues.Issue.query(
+            issues.Issue.volume == subscription.volume
+        ).order(-issues.Issue.pubdate)
+        last_issue = yield issue_query.get_async()
+        logging.debug('Most recent pull for %r is %r[%s]',
+                      subscription.key, last_issue.key, last_issue.pubdate)
+        if last_issue:
+            issue_age = datetime.now().date() - last_issue.pubdate
+            if issue_age.days <= 90 and not subscription.fresh:
+                logging.debug('Marking fresh: age:%r fresh:%r',
+                              issue_age, subscription.fresh)
+                subscription.fresh = True
+                changed = True
+            if subscription.fresh and issue_age.days > 90:
+                logging.debug('Marking stale: age:%r fresh:%r',
+                              issue_age, subscription.fresh)
+                subscription.fresh = False
+                changed = True
+        raise ndb.Return(changed)
+
+    @ndb.tasklet
+    def refresh_subscription(self, subscription):
+        changed = False
+        freshness_changed = yield self.check_freshness(subscription)
+        if freshness_changed:
+            changed = True
+        if changed:
+            update = yield subscription.put_async()
+            raise ndb.Return(update)
+
+    def active_shard(self):
+        if self.request.get('shard'):
+            shard = int(self.request.get('shard'))
+        else:
+            shard = current_shard()
+        return shard
+
+    def get(self):
+        shard = self.active_shard()
+        query = subscriptions.Subscription.query(
+            subscriptions.Subscription.shard == shard
+        )
+        shard_count = query.count_async()
+        updates = query.map(self.refresh_subscription)
+        update_count = sum(1 for update in updates if update)
+        shard_count = shard_count.get_result()
+        message = 'updated %d of %d subscriptions in shard %d' % (
+            update_count, shard_count, shard)
+        logging.info(message)
+        self.response.write({
+            'status': 200,
+            'message': message,
+            'total': shard_count,
+            'updated': update_count,
+            'shard': shard,
+        })
+
 
 class ReshardSubs(TaskHandler):
     @ndb.tasklet
@@ -113,7 +215,17 @@ class Validate(TaskHandler):
             'deleted': deleted,
         }))
 
+
+def current_shard(offset=0):
+    shard_key = admin.Setting.query(
+        admin.Setting.name == 'watch_shard_count')
+    shard_count = int(shard_key.get().value)
+    shard = (int(time.time() // 3600) + offset) % shard_count
+    return shard
+
 app = create_app([
+    Route('/tasks/subscriptions/queue/volumes', QueueActiveVolumes),
+    Route('/tasks/subscriptions/refresh', RefreshSubscription),
     Route('/tasks/subscriptions/reshard', ReshardSubs),
     Route('/<:batch|tasks>/subscriptions/update', UpdateSubs),
     Route('/tasks/subscriptions/validate', Validate),
