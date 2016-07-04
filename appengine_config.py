b3b02f703addfe5fbc3c4034d773f1ec2d210e96
Russell Heilling <russell@heilling.net>
2016-07-04 11:48:58 +0000

    Switch to using vendor module rather than altering sys.path

--- appengine_config.py
+++ appengine_config.py
@@ -3,9 +3,12 @@ import os
 import site
 import sys
 
+from google.appengine.ext import vendor
+
+vendor.add('lib')
+
 approot = os.path.dirname(__file__)
 sys.path.append(os.path.join(approot, 'common'))
-sys.path.append(os.path.join(approot, 'lib'))
 
 def webapp_add_wsgi_middleware(app):
   from google.appengine.ext.appstats import recording
